b456e06c1054f41d9f7a4eaa7919ad850409c09f
RubensCarvalhoRocha <rocharubens4@gmail.com>
2025-06-17 14:50:58 -0300

    feat: set up environment para a api em produção

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://physio-track-api-405e86c92952.herokuapp.com'
+    production: true,
+    api: 'https://physio-track-api-405e86c92952.herokuapp.com',
 };
