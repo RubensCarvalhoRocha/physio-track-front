a9999f1e6aba39acf4ecf786b0dccc9423c52ced
RubensCarvalhoRocha <rocharubens4@gmail.com>
2025-12-10 18:12:36 -0300

    Implementação de ___________________ refs #___ @___

--- src/app/mock-api/common/navigation/api.ts
+++ src/app/mock-api/common/navigation/api.ts
@@ -2,25 +2,44 @@ import { Injectable } from '@angular/core';
 import { cloneDeep } from 'lodash-es';
 import { FuseNavigationItem } from '@fuse/components/navigation';
 import { FuseMockApiService } from '@fuse/lib/mock-api';
-import { compactNavigation, defaultNavigation, futuristicNavigation, horizontalNavigation } from 'app/mock-api/common/navigation/data';
+import {
+    compactNavigation,
+    defaultNavigation,
+    futuristicNavigation,
+    horizontalNavigation,
+} from 'app/mock-api/common/navigation/data';
+import { AuthService } from 'app/core/auth/auth.service'; // <-- ADICIONADO
+import { User } from 'app/core/user/user.types';
+import { UserService } from 'app/core/user/user.service';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
-export class NavigationMockApi
-{
-    private readonly _compactNavigation: FuseNavigationItem[] = compactNavigation;
-    private readonly _defaultNavigation: FuseNavigationItem[] = defaultNavigation;
-    private readonly _futuristicNavigation: FuseNavigationItem[] = futuristicNavigation;
-    private readonly _horizontalNavigation: FuseNavigationItem[] = horizontalNavigation;
+export class NavigationMockApi {
+    usuario: any;
+    private readonly _compactNavigation: FuseNavigationItem[] =
+        compactNavigation;
+    private readonly _defaultNavigation: FuseNavigationItem[] =
+        defaultNavigation;
+    private readonly _futuristicNavigation: FuseNavigationItem[] =
+        futuristicNavigation;
+    private readonly _horizontalNavigation: FuseNavigationItem[] =
+        horizontalNavigation;
 
     /**
      * Constructor
      */
-    constructor(private _fuseMockApiService: FuseMockApiService)
-    {
+    constructor(
+        private _fuseMockApiService: FuseMockApiService,
+        private _authService: AuthService, // <-- ADICIONADO
+        private _userService: UserService // <-- ADICIONADO
+    ) {
         // Register Mock API handlers
         this.registerHandlers();
+        this._userService.user$.subscribe((user) => {
+            this.usuario = user;
+            console.log('Usuário retornado pelo UserService:', this.usuario);
+        });
     }
 
     // -----------------------------------------------------------------------------------------------------
@@ -30,55 +49,71 @@ export class NavigationMockApi
     /**
      * Register Mock API handlers
      */
-    registerHandlers(): void
-    {
+    registerHandlers(): void {
         // -----------------------------------------------------------------------------------------------------
         // @ Navigation - GET
         // -----------------------------------------------------------------------------------------------------
-        this._fuseMockApiService
-            .onGet('api/common/navigation')
-            .reply(() => {
-
-                // Fill compact navigation children using the default navigation
-                this._compactNavigation.forEach((compactNavItem) => {
-                    this._defaultNavigation.forEach((defaultNavItem) => {
-                        if ( defaultNavItem.id === compactNavItem.id )
-                        {
-                            compactNavItem.children = cloneDeep(defaultNavItem.children);
-                        }
-                    });
-                });
-
-                // Fill futuristic navigation children using the default navigation
-                this._futuristicNavigation.forEach((futuristicNavItem) => {
-                    this._defaultNavigation.forEach((defaultNavItem) => {
-                        if ( defaultNavItem.id === futuristicNavItem.id )
-                        {
-                            futuristicNavItem.children = cloneDeep(defaultNavItem.children);
-                        }
-                    });
-                });
-
-                // Fill horizontal navigation children using the default navigation
-                this._horizontalNavigation.forEach((horizontalNavItem) => {
-                    this._defaultNavigation.forEach((defaultNavItem) => {
-                        if ( defaultNavItem.id === horizontalNavItem.id )
-                        {
-                            horizontalNavItem.children = cloneDeep(defaultNavItem.children);
-                        }
-                    });
-                });
-
-                // Return the response
-                return [
-                    200,
-                    {
-                        compact   : cloneDeep(this._compactNavigation),
-                        default   : cloneDeep(this._defaultNavigation),
-                        futuristic: cloneDeep(this._futuristicNavigation),
-                        horizontal: cloneDeep(this._horizontalNavigation)
+        this._fuseMockApiService.onGet('api/common/navigation').reply(() => {
+            // Fill compact navigation children using the default navigation
+            this._compactNavigation.forEach((compactNavItem) => {
+                this._defaultNavigation.forEach((defaultNavItem) => {
+                    if (defaultNavItem.id === compactNavItem.id) {
+                        compactNavItem.children = cloneDeep(
+                            defaultNavItem.children
+                        );
                     }
-                ];
+                });
             });
+
+            // Fill futuristic navigation children using the default navigation
+            this._futuristicNavigation.forEach((futuristicNavItem) => {
+                this._defaultNavigation.forEach((defaultNavItem) => {
+                    if (defaultNavItem.id === futuristicNavItem.id) {
+                        futuristicNavItem.children = cloneDeep(
+                            defaultNavItem.children
+                        );
+                    }
+                });
+            });
+
+            // Fill horizontal navigation children using the default navigation
+            this._horizontalNavigation.forEach((horizontalNavItem) => {
+                this._defaultNavigation.forEach((defaultNavItem) => {
+                    if (defaultNavItem.id === horizontalNavItem.id) {
+                        horizontalNavItem.children = cloneDeep(
+                            defaultNavItem.children
+                        );
+                    }
+                });
+            });
+
+            // ---------------------------------------------------------------------
+            // 🔥 REMOVER ITEM "pessoa" SE usuario.tipoUsuario === 'Paciente'
+            // ---------------------------------------------------------------------
+
+            if (this.usuario?.tipoUsuario === 'Paciente') {
+                // Remove 'pessoa' dos menus
+                const removeItem = (nav: FuseNavigationItem[]) => {
+                    const index = nav.findIndex((i) => i.id === 'pessoa');
+                    if (index > -1) nav.splice(index, 1);
+                };
+
+                removeItem(this._compactNavigation);
+                removeItem(this._defaultNavigation);
+                removeItem(this._futuristicNavigation);
+                removeItem(this._horizontalNavigation);
+            }
+
+            // Return the response
+            return [
+                200,
+                {
+                    compact: cloneDeep(this._compactNavigation),
+                    default: cloneDeep(this._defaultNavigation),
+                    futuristic: cloneDeep(this._futuristicNavigation),
+                    horizontal: cloneDeep(this._horizontalNavigation),
+                },
+            ];
+        });
     }
 }
